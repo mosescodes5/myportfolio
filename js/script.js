d1e480377a9a9e4a5170434c0840c68a0a820175
mosesodes55 <mosesoluwa2005@icloud.com>
2024-09-21 04:24:38 -0700

    my portofolio

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let htmlProgress = document.querySelector(".html-css"),
   htmlValue = document.querySelector(".html-progress");
 
 let htmlStartValue = 0,
-  htmlEndValue = 90,
+  htmlEndValue = 80,
   htmlspeed = 30;
 
 let progresshtml = setInterval(() => {
@@ -24,7 +24,7 @@ let javascriptProgress = document.querySelector(".javascript"),
   javascriptValue = document.querySelector(".javascript-progress");
 
 let javascriptStartValue = 0,
-  javascriptEndValue = 75,
+  javascriptEndValue = 65,
   jsspeed = 30;
 
 let progressjs = setInterval(() => {
@@ -45,7 +45,7 @@ let phpProgress = document.querySelector(".php"),
   phpValue = document.querySelector(".php-progress");
 
 let phpStartValue = 0,
-  phpEndValue = 80,
+  phpEndValue = 50,
   phpspeed = 30;
 
 let progressphp = setInterval(() => {
@@ -66,7 +66,7 @@ let reactProgress = document.querySelector(".reactjs"),
   reactValue = document.querySelector(".reactjs-progress");
 
 let reactStartValue = 0,
-  reactEndValue = 30,
+  reactEndValue = 20,
   rjsspeed = 30;
 
 let progressreact = setInterval(() => {
